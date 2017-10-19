222a220ab19b6e35bda3a41971308d7ac2c13140
tkygtr6 <tkygtr6@gmail.com>
2017-10-19 17:15:58 +0900

    Update plot.sh

--- experiment/ave.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-args = sys.argv
-
-print( (float(args[1]) - float(args[2])) / 2 )
--- experiment/plot.sh
+++ experiment/plot.sh
@@ -7,7 +7,6 @@ else
     result_file_name=$1
 fi
 
-
 i_max=$(cat ${result_file_name}| awk 'BEGIN{m=-1000000}{if(m<$2) m=$2} END{print m}')
 echo "i_max":  $i_max
 
@@ -20,11 +19,17 @@ echo "omega_max":  $omega_max
 omega_min=$(cat ${result_file_name}| awk 'BEGIN{m=1000000}{if(m>$3) m=$3} END{print m}')
 echo "omega_min":  $omega_min
 
-python ave.py $i_max $i_min
-python ave.py $omega_max $omega_min
+echo "i_length: " + $(echo "($i_max - $i_min) * 0.5" | bc )
+echo "omega_length: " + $(echo "($omega_max - $omega_min) * 0.5" | bc )
 
-echo plot '"'${result_file_name}'"' u 1:3 every 10 w l > gnuplot
-echo replot '"'${result_file_name}'"' u 1:4  every 10 w l >> gnuplot
+column_num=$(head  -1 ${result_file_name} | awk '{printf("%s",NF)}')
+if [ $column_num -eq 4]; then
+	echo plot '"'${result_file_name}'"' u 1:3 every 10 w l > gnuplot
+	echo replot '"'${result_file_name}'"' u 1:4  every 10 w l >> gnuplot
+else
+	echo plot '"'${result_file_name}'"' u 1:2 every 10 w l > gnuplot
+	echo replot '"'${result_file_name}'"' u 1:3  every 10 w l >> gnuplot
+fi
 echo "set grid" >> gnuplot
 echo "pause 500" >> gnuplot
 cat gnuplot | gnuplot
