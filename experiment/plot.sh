8df02a8039ccbce7caf32cfbe7d5a590a328a352
tkygtr6 <tkygtr6@gmail.com>
2017-10-19 21:05:22 +0900

    fix

--- experiment/plot.sh
+++ experiment/plot.sh
@@ -23,24 +23,33 @@ echo "omega_length: " + $(echo "($omega_max - $omega_min) * 0.5" | bc )
 # 結果をgnuplotで出力する。
 output_file_name=$(echo ${result_file_name} | sed 's/.data/.pdf/g')
 column_num=$(head  -1 ${result_file_name} | awk '{printf("%s",NF)}')
-if [ $column_num -eq 4]; then
-	echo plot '"'${result_file_name}'"' u 1:3 every 10 w l title '"current [A]"' > gnuplot
-	echo replot '"'${result_file_name}'"' u 1:4 every 10 w l title '"position [rad]"' >> gnuplot
+if [ $column_num -eq 4 ]; then
+	cat << EOF > gnuplot
+plot "${result_file_name}" u 1:3 every 10 w l title "current [A]"
+replot "${result_file_name}" u 1:4 every 10 w l title "position [rad]"
+EOF
 else
-	echo plot '"'${result_file_name}'"' u 1:2 every 10 w l title '"current[A]"'  > gnuplot
-	echo replot '"'${result_file_name}'"' u 1:3 every 10 w l title '"position [rad]"'  >> gnuplot
+	cat << EOF > gnuplot
+plot "${result_file_name}" u 1:2 every 10 w l title "current [A]"
+replot "${result_file_name}" u 1:3 every 10 w l title  "position [rad]"
+EOF
 fi
-echo set tics font '"'Helvetica,20'"'  >>gnuplot  # 目盛りのフォントの変更
-echo set xlabel font '"'Helvetica,20'"' >> gnuplot # xlabelのフォントの変更
-echo set ylabel font '"'Helvetica,20'"' >> gnuplot # ylabelのフォントの変更
-echo set key font '"'Helvetica,20'"'  >> gnuplot  # 凡例のフォントの変更
-echo set grid >> gnuplot
-echo set xlabel "'"time[s]"'" >> gnuplot
-echo set ylabel "'"current[A]/position[rad]"'" >> gnuplot
-echo replot >> gnuplot
-# echo "pause 500" >> gnuplot
-echo set terminal pdfcairo >> gnuplot
-echo set output '"'${output_file_name}'"' >> gnuplot
-echo replot >> gnuplot
-echo unset output >> gnuplot 
+cat << EOF >> gnuplot
+set tics font "Helvetica,20"
+set xlabel font "Helvetica,20"
+set ylabel font "Helvetica,20"
+set key font "Helvetica,20"
+set grid
+set xlabel "time[s]"
+set ylabel "current[A]/position[rad]"
+replot
+set terminal pdfcairo
+set lmargin 10
+set rmargin 4
+set tmargin 3
+set bmargin 5
+set output "${output_file_name}"
+replot
+unset output
+EOF
 cat gnuplot | gnuplot
